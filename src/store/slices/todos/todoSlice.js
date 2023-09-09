28de351be5132e3231d8f498fe859050d1f4d6e3
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-09 11:33:38 -0500

    ✨feature: create todo slice

---
+++ src/store/slices/todos/todoSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: {},
+  reducers: {
+
+  }
+});
+
+export const { } = todosSlice.actions;
+
+export default todosSlice.reducer;
