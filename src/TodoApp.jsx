7346b1d7352725a5a0a16bd4b3e26d278a4fe37e
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-12 12:05:49 -0500

    ✨feature: Get Todo By Id with RTK Query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,18 +1,44 @@
-import { useGetTodosQuery } from "./store/apis"
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis"
 
 
 export const TodoApp = () => {
 
-  const { data: todos = [], isLoading } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1);
+
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  }
+
+  const prevTodo = () => {
+    if( todoId > 1 ) {
+      setTodoId(todoId - 1);
+    }
+  }
+
 
   return (
     <>
       <h1>Todo - RTK Query</h1>
       <hr />
       <h4>isLoading: { isLoading ? 'True' : 'False'}</h4>
-      <pre>...</pre>
+      <pre style={{
+        textAlign: 'left',
+        fontSize: '1.2rem',
+        background: '#eee',
+        padding: '1rem',
+        borderRadius: '5px',
+        color: 'black'
+      }}>{
+        JSON.stringify(todo, null, 2)
+      }</pre>
+      <button onClick={prevTodo} > {'<-'} </button>
+      <button onClick={nextTodo}> {'->'} </button>
 
-      <ul style={
+      {/* <ul style={
         {
           display: 'flex',
           flexDirection: 'column',
@@ -27,8 +53,8 @@ export const TodoApp = () => {
             {todo.title}
           </li>
         ))}
-      </ul>
-      <button>Next Todo</button>
+      </ul> */}
+
     </>
   )
 }
