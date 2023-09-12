82f01c4d052827a1ab1f7554bb673f646fb14d55
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-12 11:11:19 -0500

    ✨feature: Init RTK Query

---
+++ src/store/apis/todo/todoApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const todoApi = createApi({
+  reducerPath: 'todo',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com'
+  }),
+
+  endpoints: (builder) => ({
+
+    getTodos: builder.query({
+      query: () => '/todos',
+    })
+
+  }),
+
+});
+
+export const { useGetTodosQuery } = todoApi;
